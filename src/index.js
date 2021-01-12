858aa94e14b66a774877a95f400b268866aabcf4
Frazer Smith <frazerry@outlook.com>
2021-01-12 10:48:52 +0000

    refactor: remove `nocache` dependency; set headers directly

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const fp = require("fastify-plugin");
-const nocache = require("nocache");
-
 /**
  * @author Frazer Smith
- * @description Simple wrapper that adds an `onRequest` hook to call `nocache`, which turns off
- * client-side caching by setting the relevant response headers.
- * See https://github.com/helmetjs/nocache
+ * @description Simple wrapper that adds an `onRequest` hook to disable client-side caching
+ * by setting the relevant response headers.
  * @param {Function} server - Fastify instance.
  */
 async function plugin(server) {
-	const middleware = nocache();
-
 	server.addHook("onRequest", (req, reply, next) => {
-		middleware(req.raw, reply.raw, next);
+		reply.headers({
+			"Surrogate-Control": "no-store",
+			"Cache-Control":
+				"no-store, no-cache, must-revalidate, proxy-revalidate",
+			Pragma: "no-cache",
+			Expires: "0",
+		});
+		next();
 	});
 }
 
