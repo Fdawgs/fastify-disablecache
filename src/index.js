952508663db833a8dc7b72acb9f53a2bf775c6eb
Frazer Smith <frazer.dev@outlook.com>
2022-03-28 15:40:33 +0100

    style(index): use `res` instead of `reply` for response object

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const fp = require("fastify-plugin");
  * @param {object} server - Fastify instance.
  */
 async function plugin(server) {
-	server.addHook("onRequest", async (req, reply) => {
-		reply.headers({
+	server.addHook("onRequest", async (req, res) => {
+		res.headers({
 			"Cache-Control": "no-store, max-age=0, must-revalidate",
 			Expires: "0",
 			Pragma: "no-cache",
