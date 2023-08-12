9c6f9a3bda9ea60a560594c1a1c3a3b208d1fe81
Frazer Smith <frazer.dev@outlook.com>
2023-08-12 14:40:51 +0100

    chore: add jsconfig; type check js

--- src/index.test.js
+++ src/index.test.js
@@ -5,6 +5,9 @@ const plugin = require(".");
 
 describe("Disablecache plugin", () => {
 	describe("Response headers", () => {
+		/**
+		 * @type {Fastify.FastifyInstance}
+		 */
 		let server;
 
 		beforeAll(async () => {
