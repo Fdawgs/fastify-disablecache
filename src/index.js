b2fd7b9099a691b9df1dd649f312f1ea27da50da
Frazer Smith <frazer.dev@icloud.com>
2025-08-25 13:35:47 +0100

    perf(index): hoist `setCacheHeaders` to module scope (#336)

--- src/index.js
+++ src/index.js
@@ -11,15 +11,22 @@ const CACHE_HEADERS = {
 
 /**
  * @author Frazer Smith
+ * @description Adds cache disabling headers to the response.
+ * @type {import("fastify").onRequestHookHandler}
+ */
+function setCacheHeaders(_req, res, next) {
+	res.headers(CACHE_HEADERS);
+	next();
+}
+
+/**
+ * @author Frazer Smith
  * @description Simple plugin that adds an `onRequest` hook to disable client-side caching
  * by setting the relevant response headers.
  * @type {import("fastify").FastifyPluginCallback}
  */
 function fastifyDisablecache(server, _options, done) {
-	server.addHook("onRequest", (_req, res, next) => {
-		res.headers(CACHE_HEADERS);
-		next();
-	});
+	server.addHook("onRequest", setCacheHeaders);
 	done();
 }
 
