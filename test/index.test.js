caa50d238aaaecf6d914faf9ac33de1f856501dc
Frazer Smith <frazer.dev@icloud.com>
2025-11-19 10:24:30 +0000

    chore: move tests from src to test dir

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ const assert = require("node:assert");
 // eslint-disable-next-line n/no-unsupported-features/node-builtins -- Tests, not in distributed code
 const { after, before, describe, it } = require("node:test");
 const Fastify = require("fastify");
-const plugin = require(".");
+const plugin = require("../src");
 
 /** @typedef {import('node:test').TestContext} TestContext */
 
